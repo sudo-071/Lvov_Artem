a3cab506b25ed2ff726b5683cecac0816cd3fb5b
sudo-071 <ArtemLvov81@gmail.com>
2020-12-20 17:28:44 +0200

    all

--- Lab_05/src/main.c
+++ Lab_05/src/main.c
@@ -2,11 +2,29 @@ int main()
 {
 	int src = 13;				/*defining the source number*/
 	int res = 0;				/*defining variable for GF*/			
-	for (int i = 1; i < src; i++){	/*cycle to calculate the GF (using "for")*/
+	// for (int i = 1; i < src; i++){	/*cycle to calculate the GF (using "for")*/
+	// 	if (src % i == 0){
+	// 		res = i;		
+	//	}
+	// }
+	
+	
+	// int i = 1;				/*cycle to calculate the GF (using "while")*/
+	// while (i < src){			
+	// 	if (src % i ==0){
+	// 		res = i;
+	// 	}
+	// 	i++;
+	// }
+	
+	
+	int i = 1;				/*cycle to calculate the GF (using "do...while")*/
+	do{					
 		if (src % i == 0){
-			res = i;		
+			res = i;
 		}
-	}
+		i++;
+	}while (i < src);
 	if (res == 1){ 			/*checking that number is prime or not*/
 		/*number is prime*/
 	}
